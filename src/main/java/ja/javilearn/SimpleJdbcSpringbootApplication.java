a30d57048d55cd1a54ec83c0d0a230e3dc078547
MocoNinja <javiergonzaleztello@gmail.com>
2019-04-14 23:40:34 +0200

    Add SpringBoot nature to project
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

---
+++ src/main/java/ja/javilearn/SimpleJdbcSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ja.javilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJdbcSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJdbcSpringbootApplication.class, args);
+	}
+
+}
